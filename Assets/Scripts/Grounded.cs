d07db699f4a6257644b431bec9e8097d726a2156
unknown <adntouch@gmail.com>
2020-03-10 11:55:10 +0100

    Movement Character

---
+++ Assets/Scripts/Grounded.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grounded : MonoBehaviour
+{
+    private Character CharacterInstance;
+    // Start is called before the first frame update
+    void Start()
+    {
+        CharacterInstance = transform.parent.GetComponent<Character>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        CharacterInstance.SetIsGrounded(true);
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        CharacterInstance.SetIsGrounded(false);
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : Character
+{
+
+    private Vector2 Movement;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Init();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (IsGrounded)
+        {
+            Movement.x = Input.GetAxis("Horizontal");
+            SetBoolAnim("IsRunning", Movement.x>0 || Movement.x<0);
+        }
+        
+        Movement.y = (OnJump)? 1 : (IsGrounded)? 0 : -1;
+
+        if (CanJump && Input.GetButtonDown("Jump"))
+        {
+            Jump();
+            
+        }
+        else if (OnJump && transform.position.y >= MaxJumpHeight)
+        {
+            EndJump();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        CalcCurrentSpeed();
+        
+        Move(Movement);
+    }
+}
