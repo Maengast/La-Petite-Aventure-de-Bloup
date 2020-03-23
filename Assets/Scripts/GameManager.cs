c3491d4813276e4da4febaea2af9326cd09de07f
Maengast <adntouch@gmail.com>
2020-03-23 14:37:06 +0100

    wip ui & player prefab

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,6 +7,9 @@ public class GameManager : Singleton<GameManager>
 {
 	private UIManager _uiManager;
 
+	private int currentLevelNumber = 1;
+	private int maxLevelNumber;
+	
 	private void Start()
 	{
 		_uiManager = UIManager.Instance;
@@ -18,31 +21,33 @@ public class GameManager : Singleton<GameManager>
 	{
 		MainMenu,
 		LevelSelection,
-		LoadGame,
+		LoadLevel,
 		InGame,
 		GameResults
+	}
+	  
+	public enum LevelState
+	{
+		Close,
+		New,
+		Played
 	}
 
 	private GameState CurrentGameState;
 
 	public void SwitchGameState(GameState newGameState)
 	{
-
 		switch (newGameState)
 		{
 			case GameState.MainMenu :
 				//open scene main menu
 				break;
 			case GameState.LevelSelection:
-				//open scene main menu and switch panel level selection
+				//open scene main menu if old game state not main menu
 				break;
-			case GameState.LoadGame:
+			case GameState.LoadLevel:
 				//open scene in game
-				break;
-			case GameState.InGame:
-				break;
-			case GameState.GameResults:
-				//show results
+				//tell ui display level number
 				break;
 		}
 		
@@ -55,17 +60,35 @@ public class GameManager : Singleton<GameManager>
 		return CurrentGameState;
 	}
 
+	public int GetCurrentLevel()
+	{
+		return currentLevelNumber;
+	}
+
 	public void InstantiatePlayer()
 	{
 		
 	}
 
+	public LevelState GetLevelState(int levelNumber)
+	{
+		if (levelNumber < currentLevelNumber)
+		{
+			return LevelState.Played;
+		}
+
+		if (levelNumber == currentLevelNumber)
+		{
+			return LevelState.New;
+		}
+
+		return LevelState.Close;
+	}
+
 	public void LoadLevel(int levelNumber)
 	{
-		//save level number
-		SwitchGameState(GameState.LoadGame);
-		//Tell ui manager to switch to level panel and display level number
-		
+		currentLevelNumber = levelNumber;
+		SwitchGameState(GameState.LoadLevel);
 	}
 
 	public void EndLoadLevel()
@@ -73,15 +96,10 @@ public class GameManager : Singleton<GameManager>
 		SwitchGameState(GameState.InGame);
 	}
 	
-	
-	public void StartGame()
-	{
-		
-	}
-
 	public void EndGame(bool gameOver)
 	{
 		SwitchGameState(GameState.GameResults);
+		//ui display game result
 	}
 	
 }
---
+++ Assets/Scripts/UI/UIButton.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Sprites;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UIElements;
+
+public class UIButton : MonoBehaviour
+{
+
+	public ButtonSpriteState SpriteState;
+
+	protected UIManager uiManager;
+	public UnityEvent ButtonEvent;
+	
+	private void Start()
+	{
+		uiManager = UIManager.Instance;
+	}
+
+	// Start is called before the first frame update
+    private void OnMouseOver()
+    {
+
+    }
+
+    private void OnMouseExit()
+    {
+	    
+    }
+
+    private void OnMouseUpAsButton()
+    {
+	    ButtonEvent.Invoke();
+    }
+
+}
+
+[Serializable]
+public class ButtonSpriteState
+{
+	public Sprite DefaultSprite;
+	public Sprite HighlightSprite;
+	public Sprite PressedSprite;
+}
